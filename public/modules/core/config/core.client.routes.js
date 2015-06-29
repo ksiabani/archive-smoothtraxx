624b9026ee971fbb911a28bdf2270d6de6475b6c
Kostas Siabanis <kostas.siabanis@gmail.com>
2015-06-29 12:55:57 +0300

    Added radio pages

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -2,23 +2,27 @@
 
 // Setting up route
 angular.module('core').config(['$stateProvider', '$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		// Redirect to home view when route not found
-		$urlRouterProvider.otherwise('/');
+  function ($stateProvider, $urlRouterProvider) {
+    // Redirect to home view when route not found
+    $urlRouterProvider.otherwise('/');
 
-		// Home state routing
-		$stateProvider.
-		state('home', {
-			url: '/',
-			templateUrl: 'modules/core/views/home.client.view.html'
-		}).
-		state('music', {
-			url: '/music',
-			templateUrl: 'modules/core/views/music.client.view.html'
-		}).
-		state('radio', {
-			url: '/radio',
-			templateUrl: 'modules/core/views/radio.client.view.html'
-		});
-	}
+    // Home state routing
+    $stateProvider.
+        state('home', {
+          url: '/',
+          templateUrl: 'modules/core/views/home.client.view.html'
+        }).
+        state('music', {
+          url: '/music',
+          templateUrl: 'modules/core/views/music.client.view.html'
+        }).
+        state('radio', {
+          url: '/radio',
+          templateUrl: 'modules/core/views/radio.client.view.html'
+        }).
+        state('viewRadio', {
+          url: '/radio/:radioId',
+          templateUrl: 'modules/core/views/view-radio.client.view.html'
+        });
+  }
 ]);
