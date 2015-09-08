664ec528c7363d4ae1808e8a0ecca75646536d0e
Kostas <kostas.siabanis@gmail.com>
2015-09-08 14:27:16 +0300

    Add assets for production

--- config/env/production.js
+++ config/env/production.js
@@ -3,25 +3,41 @@
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/groovity-library',
 	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-			],
-			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
-			]
-		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
+	  lib: {
+		css: [
+		  'public/lib/bootstrap/dist/css/bootstrap.css',
+		  'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+		  'public/lib/font-awesome/css/font-awesome.css',
+		  'public/lib/ionicons/css/ionicons.css',
+		  'public/lib/ng-mfb/mfb/dist/mfb.css'
+		],
+		js: [
+		  'public/lib/angular/angular.js',
+		  'public/lib/angular-resource/angular-resource.js',
+		  'public/lib/angular-cookies/angular-cookies.js',
+		  'public/lib/angular-animate/angular-animate.js',
+		  'public/lib/angular-touch/angular-touch.js',
+		  'public/lib/angular-sanitize/angular-sanitize.js',
+		  'public/lib/angular-ui-router/release/angular-ui-router.js',
+		  'public/lib/angular-ui-utils/ui-utils.js',
+		  'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+		  'public/lib/videogular/videogular.js',
+		  'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.js',
+		  'public/lib/color-thief/src/color-thief.js',
+		  'public/lib/angular-colorthief/angular-colorthief.js',
+		  'public/lib/angular-deckgrid/angular-deckgrid.js',
+		  'public/lib/ng-mfb/src/mfb-directive.js'
+		]
+	  },
+	  css: [
+		'public/modules/**/css/*.css'
+	  ],
+	  js: [
+		'public/config.js',
+		'public/application.js',
+		'public/modules/*/*.js',
+		'public/modules/*/*[!tests]*/*.js'
+	  ]
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
